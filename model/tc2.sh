4eb6071d2b6347bb3e8c9bb4ef5d5d389b7f63ab
Manish V Badarkhe <Manish.Badarkhe@arm.com>
2024-01-29 18:24:38 +0000

    tc2: update TC2 model parameters as per recent model 11.23/17
    Signed-off-by: Manish V Badarkhe <Manish.Badarkhe@arm.com>
    Change-Id: Id304f40eefc5a7050e58c8d2f4e023fc9de614a3
    (cherry picked from commit bc0a59ff5d5d3fac3b24b2c6e2b5c83357e916db)

--- model/tc2.sh
+++ model/tc2.sh
@@ -1,12 +1,10 @@
 #!/usr/bin/env bash
 #
-# Copyright (c) 2022, Arm Limited. All rights reserved.
+# Copyright (c) 2022-2024, Arm Limited. All rights reserved.
 #
 # SPDX-License-Identifier: BSD-3-Clause
 #
-
-set_model_path "$warehouse/SysGen/SubSystemModels/11.20/24/models/$model_flavour/FVP_TC2"
-
+set_model_path "$warehouse/SysGen/SubSystemModels/11.23/17/models/$model_flavour/FVP_TC2"
 cat <<EOF >"$model_param_file"
 -C css.terminal_uart_ap.start_port=5000
 -C css.terminal_uart1_ap.start_port=5001
@@ -14,17 +12,13 @@ cat <<EOF >"$model_param_file"
 -C soc.terminal_s1.start_port=5003
 -C board.terminal_0.start_port=5004
 -C board.terminal_1.start_port=5005
-
-${fip_bin+-C board.flashloader0.fname=$host_flash_fip_bin}
-${initrd_bin+--data board.dram=$initrd_bin@${initrd_addr:?}}
-${kernel_bin+--data board.dram=$kernel_bin@${kernel_addr:?}}
-${uart0_out+-C soc.pl011_uart0.out_file=$uart0_out}
-${uart0_out+-C soc.pl011_uart0.unbuffered_output=1}
-${uart1_out+-C soc.pl011_uart1.out_file=$uart1_out}
-${uart1_out+-C soc.pl011_uart1.unbuffered_output=1}
--C displayController=2
-${rss_rom_bin+--data css.rss.cpu=$rss_rom_bin@${rss_rom_addr:?}}
+${fip_gpt_bin+-C board.flashloader0.fname=$fip_gpt_bin}
+${tc_fitimage_bin+--data board.dram=$tc_fitimage_bin@0x20000000}
 ${vmmaddrwidth+-C css.rss.VMADDRWIDTH=$vmmaddrwidth}
-${rvbaddr_lw+-C css.scp.c0_pik.rvbaraddr_lw=$rvbaddr_lw}
-${rvbaddr_up+-C css.scp.c0_pik.rvbaraddr_up=$rvbaddr_up}
+${rss_rom_bin+-C css.rss.rom.raw_image=$rss_rom_bin}
+-C displayController=2
+-C css.rss.CMU0_NUM_DB_CH=16
+-C css.rss.CMU1_NUM_DB_CH=16
+${rss_encrypted_cm_provisioning_bundle_0_bin+--data css.rss.sram0=${rss_encrypted_cm_provisioning_bundle_0_bin}@0x400}
+${rss_encrypted_dm_provisioning_bundle_bin+--data css.rss.sram1=${rss_encrypted_dm_provisioning_bundle_bin}@0x80000}
 EOF
