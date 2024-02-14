99ee1575bd0fe34e0605c589663a2eb4fb9d5975
Rohit Mathew <Rohit.Mathew@arm.com>
2024-02-14 22:16:00 +0000

    refactor(sgi): rename variable name and path for prebuilts
    Currently, reference design platforms such as RD-N1-Edge, RD-V1, and
    SGI-575 are collectively known as "sgi platforms". SGI stands for System
    Guidance Infrastructure and was initially associated with the SGI-575
    platform. However, subsequent platforms released were under the Neoverse
    reference design product name. To align with the Neoverse reference
    design nomenclature, source files for these platforms for both Trusted
    Firmware and Trusted Firmware Tests have moved away from SGI to Neoverse
    RD/NRD/nrd in their naming convention.
    
    As of now, the prebuilts for "sgi platforms" are stored within
    $css_downloads/sgi/<platform_name> referenced by the variable
    "sgi_prebuilts". In order to aid the move from "sgi", change the
    prebuilt path to $tfa_downloads/neoverse_rd/<platform_name>.
    Additionally, rename the prebuilt variable prefix to "nrd" from "sgi".
    
    Signed-off-by: Rohit Mathew <Rohit.Mathew@arm.com>
    Change-Id: I01a7fd6ad7e56e5690dc30adc0680f25ef450972

--- rdv1_utils.sh
+++ rdv1_utils.sh
@@ -1,20 +1,20 @@
 #!/usr/bin/env bash
 #
-# Copyright (c) 2020-2023, Arm Limited. All rights reserved.
+# Copyright (c) 2020-2024, Arm Limited and Contributors. All rights reserved.
 #
 # SPDX-License-Identifier: BSD-3-Clause
 #
 
 source "$ci_root/fvp_utils.sh"
 
-sgi_prebuilts="${sgi_prebuilts:-$css_downloads/sgi/rdv1}"
+nrd_prebuilts="${nrd_prebuilts:-$tfa_downloads/neoverse_rd/rdv1}"
 
 # Pre-built SCP/MCP binaries
 scp_mcp_prebuilts="${scp_mcp_prebuilts:-$scp_mcp_downloads/rdv1/release}"
 
 
-kernel_list[sgi-busybox]="$sgi_prebuilts/Image"
-initrd_list[sgi-ramdisk]="$sgi_prebuilts/ramdisk-busybox.img"
+kernel_list[sgi-busybox]="$nrd_prebuilts/Image"
+initrd_list[sgi-ramdisk]="$nrd_prebuilts/ramdisk-busybox.img"
 
 scp_ram_addr=0x0bd80000
 mcp_ram_addr=0x0BF80000
--- sgi575_utils.sh
+++ sgi575_utils.sh
@@ -1,19 +1,19 @@
 #!/usr/bin/env bash
 #
-# Copyright (c) 2019-2021 Arm Limited. All rights reserved.
+# Copyright (c) 2019-2024 Arm Limited and Contributors. All rights reserved.
 #
 # SPDX-License-Identifier: BSD-3-Clause
 #
 
 source "$ci_root/fvp_utils.sh"
 
-sgi_prebuilts="${sgi_prebuilts:-$css_downloads/sgi/sgi575}"
+nrd_prebuilts="${nrd_prebuilts:-$tfa_downloads/neoverse_rd/sgi575}"
 
 # Pre-built SCP/MCP binaries
 scp_mcp_prebuilts="${scp_mcp_prebuilts:-$scp_mcp_downloads/sgi575/release}"
 
-kernel_list[sgi-busybox]="$sgi_prebuilts/Image"
-initrd_list[sgi-ramdisk]="$sgi_prebuilts/ramdisk-busybox.img"
+kernel_list[sgi-busybox]="$nrd_prebuilts/Image"
+initrd_list[sgi-ramdisk]="$nrd_prebuilts/ramdisk-busybox.img"
 
 scp_ram_addr=0x0bd80000
 mcp_ram_addr=0x0be00000
