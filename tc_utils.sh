c50dc7f29641388e6f81097ae5fc7ca750298d1c
David Vincze <david.vincze@arm.com>
2024-02-23 16:26:59 +0100

    tc2: update rss_revision for DPE support
    Update rss_revision to use new pre-built RSS and SCP binaries
    with DICE Protection Environment support.
    
    Change-Id: I085d533aacbe657441f276a143d2339bf0156e32
    Signed-off-by: David Vincze <david.vincze@arm.com>

--- tc_utils.sh
+++ tc_utils.sh
@@ -24,7 +24,7 @@ rvbaddr_up=0x0000
 # AP bl1 0x00 is mapped to 0x70000000 in RSS memory map
 ap_bl1_flash_load_addr=0x70000000
 ap_bl1_flash_size=0x20000
-rss_revision="4ab7a20"
+rss_revision="4ab7a20d"
 
 # Hafnium build repo containing Secure hafnium binaries
 spm_secure_out_dir=secure_tc_clang
