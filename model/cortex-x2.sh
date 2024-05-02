ea9121d0d6a81d2aa0fc90d39f06f5caeaa95837
Paul Sokolovsky <paul.sokolovsky@linaro.org>
2024-05-02 14:35:04 +0700

    model: Don't include inline comments in model params
    This causes misinterpretation of the following params. Move comments to
    separate lines outside the param definition.
    
    Signed-off-by: Paul Sokolovsky <paul.sokolovsky@linaro.org>
    Change-Id: Ic4e38b281f291a7fa4c5b5edae9a851f56e0fe50
    (cherry picked from commit 7298d79d73b825d3963522f6e35f79548e669b75)

--- model/cortex-x2.sh
+++ model/cortex-x2.sh
@@ -15,6 +15,8 @@ cat <<EOF >>"${model_param_file}"
 -C bp.terminal_2.start_port=5002
 -C bp.terminal_3.start_port=5003
 
--C pctl.use_in_cluster_ppu=true # Needed since 11.22 to respect pctl.startup
--C cluster0.core_power_on_by_default=false # Needed since 11.22 to respect pctl.startup
+-C pctl.use_in_cluster_ppu=true
+-C cluster0.core_power_on_by_default=false
 EOF
+#-C pctl.use_in_cluster_ppu=true - Needed since 11.22 to respect pctl.startup
+#-C cluster0.core_power_on_by_default=false - Needed since 11.22 to respect pctl.startup
