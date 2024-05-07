c4a7a27e76bc4507bd548c20f91bbc72a3cc7ea0
Chris Kay <chris.kay@arm.com>
2024-05-07 11:50:52 +0000

    fix(e1x2): ensure UARTs ports are assigned explicitly
    The network ports which the model uses for UART output must be assigned
    explicitly so that our LAVA job configurations can predict which ports
    to connect to ahead of time.
    
    Change-Id: Iabd7c942dfc7a25954f57ba09399e71e8d004882
    Signed-off-by: Chris Kay <chris.kay@arm.com>
    (cherry picked from commit a70bc3569c35ee334fe03c8a7d9ce4e635f835aa)

--- model/neoverse_e1x2.sh
+++ model/neoverse_e1x2.sh
@@ -9,6 +9,13 @@ set_model_path "$warehouse/SysGen/Models/$model_version_11_24/$model_build_11_24
 
 source "$ci_root/model/fvp_common.sh"
 
+cat <<EOF >>"${model_param_file}"
+-C bp.terminal_0.start_port=5000
+-C bp.terminal_1.start_port=5001
+-C bp.terminal_2.start_port=5002
+-C bp.terminal_3.start_port=5003
+EOF
+
 # Base address for each redistributor
 if [ "$gicd_virtual_lpi" = "1" ]; then
 	cat <<EOF >>"$model_param_file"
