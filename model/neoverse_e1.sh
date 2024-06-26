c707d3a6d7f9a3074ccfcf63619a29532739cbbf
Govindraj Raja <govindraj.raja@arm.com>
2024-06-26 17:34:56 -0500

    fvp: cleanup neoverse_e1 model usage
    Model option 'gicd_virtual_lpi' is not been used by neoverse_e1 FVP
    model, so remove the unused option which is not been set by run_config
    and remove duplicated model files.
    
    This patch is direct outcome from code review comments from here:
    https://review.trustedfirmware.org/c/ci/tf-a-ci-scripts/+/29474/3?tab=comments
    
    Signed-off-by: Govindraj Raja <govindraj.raja@arm.com>
    Change-Id: I69c2b19a72232c4d2a1bc7134f4381f1142b783d
    (cherry picked from commit 820d06ac13832c10a016ce214c510f55e69139eb)

--- model/neoverse_e1.sh
+++ model/neoverse_e1.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #
-# Copyright (c) 2020-2022, Arm Limited. All rights reserved.
+# Copyright (c) 2020-2024, Arm Limited. All rights reserved.
 #
 # SPDX-License-Identifier: BSD-3-Clause
 #
@@ -15,11 +15,3 @@ cat <<EOF >>"${model_param_file}"
 -C bp.terminal_2.start_port=5002
 -C bp.terminal_3.start_port=5003
 EOF
-
-# Base address for each redistributor
-if [ "$gicd_virtual_lpi" = "1" ]; then
-	cat <<EOF >>"$model_param_file"
--C gic_distributor.reg-base-per-redistributor=0.0.0.0=0x2f100000,0.0.0.1=0x2f140000,0.0.1.0=0x2f180000,0.0.1.1=0x2f1c0000
--C gic_distributor.print-memory-map=1
-EOF
-fi
