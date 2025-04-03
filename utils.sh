307178f81380c17095a711e4d97b14718d0e580e
Lauren Wehrmeister <lauren.wehrmeister@arm.com>
2025-04-03 12:56:54 -0500

    feat(mbedtls): update mbedtls to version 3.6.3
    Signed-off-by: Lauren Wehrmeister <lauren.wehrmeister@arm.com>
    Change-Id: Ie704f572fea53cd1ca45b3dde07bf706b1db0ba3
    (cherry picked from commit 7c76aca3f165de74e32fd6ccc6c25b343922ef33)

--- utils.sh
+++ utils.sh
@@ -623,7 +623,7 @@ scp_mcp_downloads="${scp_mcp_downloads:-$tfa_downloads/css_scp_2.11.0}"
 
 linaro_2001_release="${linaro_2001_release:-$tfa_downloads/linaro/20.01}"
 linaro_release="${linaro_release:-$linaro_2001_release}"
-mbedtls_version="${mbedtls_version:-3.6.2}"
+mbedtls_version="${mbedtls_version:-3.6.3}"
 
 # mbedTLS archive public hosting available at github.com
 mbedtls_archive="${mbedtls_archive:-https://github.com/Mbed-TLS/mbedtls/archive/refs/tags/v${mbedtls_version}.tar.gz}"
