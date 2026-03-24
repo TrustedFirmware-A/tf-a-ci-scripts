409a311973596ec773e23f052cf4ff89c78cce1b
Boyan Karatotev <boyan.karatotev@arm.com>
2026-03-24 14:49:42 +0000

    feat(c1-pro): add a C1-Pro run config to exercise pabandon independently
    Up to now the only way to exercise pabandon has been through the TC4
    build which is quite heavyweight and has a shorter lifespan than the
    regular Base models. Migrate to C1-Pro.
    
    This requires auxiliary cpu drivers to be built on the aarch64 only
    config.
    
    Signed-off-by: Boyan Karatotev <boyan.karatotev@arm.com>
    Change-Id: I3d49a2444308f09e59542ded5380d13df74b5d02

---
+++ model/c1-pro.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+#
+# Copyright (c) 2026, Arm Limited. All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+#
+
+source "$ci_root/model/fvp_common.sh"
+
+cat <<EOF >>"${model_param_file}"
+
+-C pctl.use_in_cluster_ppu=true
+-C cluster0.core_power_on_by_default=false
+EOF
